10b45eae249dc76c1ca697da171d47383dea223a
James Vuong <jvuonger@gmail.com>
2015-04-06 10:04:38 +0200

    Finish chapter 4 exercises

--- chapter_4.js
+++ chapter_4.js
@@ -137,3 +137,47 @@ function nth(list, n) {
 }
 
 console.log(nth(arrayToList([10, 20, 30]), 1)); // → 20
+
+/*
+Exercise 4: Deep Comparison
+
+Write a function, deepEqual, that takes two values and returns true only if they are 
+the same value or are objects with the same properties whose values are also equal 
+when compared with a recursive call to deepEqual.
+
+To find out whether to compare two things by identity (use the === operator for that) 
+or by looking at their properties, you can use the typeof operator. If it produces 
+"object" for both values, you should do a deep comparison. But you have to take one 
+silly exception into account: by a historical accident, typeof null also produces 
+"object". */
+
+function deepEqual(val1, val2) {
+	if (((typeof val1 == 'object') && !!val1) && ((typeof val2 == 'object') && !!val2)) {
+		if (Object.keys(val1).length != Object.keys(val2).length) {
+			return false;
+		}
+
+		for (var property in val1) {
+			if (val2.hasOwnProperty(property)) {
+				if (!deepEqual(val1[property], val2[property])) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+
+		return true;
+	} else if (val1 !== val2) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+var obj = {here: {is: "an"}, object: 2};
+
+console.log(deepEqual(obj, obj)); // → true
+console.log(deepEqual(obj, {here: 1, object: 2})); // → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2})); // → true
+

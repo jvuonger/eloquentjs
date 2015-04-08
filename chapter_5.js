c2d75579be81df1cea65aa61627ec59363d471cf
James Vuong <jvuonger@gmail.com>
2015-04-08 10:25:34 -0400

    Start chapter 5

---
+++ chapter_5.js
@@ -0,0 +1,10 @@
+/*jslint node: true */
+'use strict';
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* * Eloquent Javascript Exercises
+* * Chapter 5 - Higher-Order Functions
+* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* 
+Exercise 1: */
